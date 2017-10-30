a9de3bf1e2784c170de172b4863bffc7417bae80
Rajanshu Ujjwal <rajanshu.ujjwal@gmail.com>
2017-10-30 16:51:38 +0530

    Fix the undefined result output error

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 let button = document.querySelector('button');
 button.addEventListener("click", function() {
-    let keyword = document.querySelector('input').value;
-    let url = 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+keyword;
+  let keyword = document.querySelector('input').value;
+  let url = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&format=json&search='+keyword;
 
-    let xmlHTTP = new XMLHttpRequest();
-    xmlHTTP.open('GET', url, true);
-    xmlHTTP.send();
-    xmlHTTP.onload = function() {
-        if (xmlHTTP.status === 200) {
-            let recieved = xmlHTTP.response;
-            let api = JSON.parse(recieved);
-            print(api);
-        } else {
-            alert("err...api error!");
-        }
+  let xmlHTTP = new XMLHttpRequest();
+  xmlHTTP.open('GET', url, true);
+  xmlHTTP.send();
+  xmlHTTP.onload = function() {
+    if (xmlHTTP.status === 200) {
+      let recieved = xmlHTTP.response;
+      let api = JSON.parse(recieved);
+        print(api);
+    } else {
+      alert("err...api error!");
     }
+  }
 });
 
 function print(api) {
+  if (api[1].length > 0) {
     for(let i = 0; i < 10; i++) {
-        let searchResult = '<li><a href='+api[3][i]+'>'+api[1][i]+'</a><br />'+api[2][i];
-        document.getElementById(i).innerHTML = searchResult;
+      let searchResult = '<li><a href='+api[3][i]+'>'+api[1][i]+'</a><br />'+api[2][i]+'</li>';
+      document.getElementById(i).innerHTML = searchResult;
     }
+  } else {
+    alert("err...No result found!")
+  }
 }
